746fc43f5cc736be5d6a7916c6b1992488c324cb
mel <mcmelon@nodiumhosting.com>
2025-09-25 10:53:24 +0200

    chore: bruh

--- projects/core/uno.config.ts
+++ projects/core/uno.config.ts
@@ -1,6 +1,10 @@
 import {defineConfig} from "unocss";
+import presetWind3 from "npm:@unocss/preset-wind3";
 
 export default defineConfig({
+    presets: [
+        presetWind3(),
+    ],
     cli: {
         entry: {
             patterns: ["./internal/dashboard/views/**/*.{html,templ,go}"],
