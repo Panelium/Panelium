3712663d20c3ea59933c01cdacaf57169c2e40b1
mel <mcmelon@nodiumhosting.com>
2026-01-22 21:12:55 +0100

    style: format files with biome

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,17 +1,17 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import {defineConfig} from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/',
+    base: "/",
     plugins: [
         react({
             babel: {
-                plugins: [['babel-plugin-react-compiler']],
+                plugins: [["babel-plugin-react-compiler"]],
             },
         }),
     ],
     build: {
-        outDir: 'dist'
-    }
-})
+        outDir: "dist",
+    },
+});
