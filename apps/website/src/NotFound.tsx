c50180c033e3f6cb3e330dcf27f7280b42a7149f
mel <mcmelon@nodiumhosting.com>
2026-01-22 20:22:46 +0100

    feat: add a 404 route to the website

---
+++ apps/website/src/NotFound.tsx
@@ -0,0 +1,7 @@
+import {Link} from "react-router";
+
+export default function NotFound() {
+    return (
+        <>Sorry! We could not find that page, would you like to <Link to={"/"}>go Home</Link> instead?</>
+    )
+}
--- apps/website/src/main.tsx
+++ apps/website/src/main.tsx
@@ -3,12 +3,14 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {BrowserRouter, Route, Routes} from "react-router";
+import NotFound from "./NotFound.tsx"
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
-                <Route path={"/"} element={<App/>}/>
+                <Route path='*' element={<NotFound/>}/>
+                <Route path='/' element={<App/>}/>
             </Routes>
         </BrowserRouter>
     </StrictMode>,
